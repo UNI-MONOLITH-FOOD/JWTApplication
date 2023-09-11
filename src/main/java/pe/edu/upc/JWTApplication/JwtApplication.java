7b0f8e06d136c6377ab43be5cc996e94a4c7f6c2
Kiridepapel <monalek0622@hotmail.com>
2023-09-11 15:50:02 -0500

    feat(dev): main files

---
+++ src/main/java/pe/edu/upc/JWTApplication/JwtApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.JWTApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/upc/JWTApplication/JwtApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.JWTApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
